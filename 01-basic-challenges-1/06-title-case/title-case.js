d389c4bae12e6ae6cc6586481fe95e8947986c67
Margus <margus.liinev@gmail.com>
2023-09-13 20:17:19 +0300

    title-case and reverse-string solutions

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(str) {
+    const newStr = str.split(' ').map((item) => {
+        return `${item.slice(0, 1).charAt(0).toUpperCase()}${item.slice(1).toLowerCase()}`;
+    });
+    return newStr.join(' ');
+}
 
 module.exports = titleCase;
--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+    const splitStr = [...str];
+    let reversed = '';
+
+    for (i = splitStr.length - 1; i >= 0; i--) {
+        reversed += splitStr[i];
+    }
+    return reversed;
+}
 
 module.exports = reverseString;
