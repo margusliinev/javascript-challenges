2009b7fe4f5f4a87396fa2aee1157b28e26d9996
Margus <margus.liinev@gmail.com>
2023-09-13 23:17:10 +0300

    solutions for palidrome, count-vowels, remove-duplicates

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+    const formatStr = str.toLowerCase();
+    let count = 0;
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i < formatStr.length; i++) {
+        const char = formatStr[i];
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,11 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    const uniqueArr = arr.reduce((acc, curr) => {
+        if (!acc.includes(curr)) {
+            acc.push(curr);
+        }
+        return acc;
+    }, []);
+    return uniqueArr;
+}
 
 module.exports = removeDuplicates;
