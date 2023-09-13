7cccf15221d6fed9664a0f0064a4447678870d0b
Margus <margus.liinev@gmail.com>
2023-09-13 19:28:25 +0300

    solutions for count-occurrences and find-max-number

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+    const stringArr = [...str];
+    const occurrences = stringArr.reduce((acc, cur) => {
+        if (cur === char) {
+            acc++;
+        }
+        return acc;
+    }, 0);
+    return occurrences;
+}
 
 module.exports = countOccurrences;
--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbers) {
+    let largestNumber = numbers[0];
+    for (i = 0; i < numbers.length; i++) {
+        if (largestNumber < numbers[i]) {
+            largestNumber = numbers[i];
+        }
+    }
+    return largestNumber;
+}
 
 module.exports = findMaxNumber;
