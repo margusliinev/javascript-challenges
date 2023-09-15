c90df8d64dc4d611e38a8fc8d771359e5f34150f
Margus <margus.liinev@gmail.com>
2023-09-16 00:02:43 +0300

    solution for find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    if (arr.length === 0) {
+        return 1;
+    }
+    const length = arr.length + 1;
+
+    const actualSum = arr.reduce((acc, cur) => {
+        acc += cur;
+        return acc;
+    }, 0);
+    const expectedSum = (length * (length + 1)) / 2;
+
+    return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
