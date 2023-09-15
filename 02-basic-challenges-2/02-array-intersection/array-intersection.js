eff9eba2eb8a84c92fdd7a3a5d00df536a93d824
Margus <margus.liinev@gmail.com>
2023-09-15 21:46:13 +0300

    added array intersection solution

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,11 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const arr3 = arr1.reduce((acc, cur) => {
+        if (arr2.includes(cur)) {
+            acc.push(cur);
+        }
+        return acc;
+    }, []);
+    return arr3;
+}
 
 module.exports = arrayIntersection;
