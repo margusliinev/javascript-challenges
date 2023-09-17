40a829b3c057bdfd90b906704a8f149a9657d98d
Margus <margus.liinev@gmail.com>
2023-09-17 23:42:49 +0300

    solution for dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,13 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numOfSimulations) {
+    const simulations = [];
+    for (let i = numOfSimulations; i > 0; i--) {
+        const dice1 = Math.ceil(Math.random() * 6);
+        const dice2 = Math.ceil(Math.random() * 6);
+        const sum = dice1 + dice2;
+        const result = sum === 7 || sum === 11 ? 'win' : sum === 2 || sum === 3 || sum === 12 ? 'lose' : 'roll again';
+        simulations.push({ dice1, dice2, sum, result });
+    }
+    return simulations;
+}
 
 module.exports = diceGameSimulation;
