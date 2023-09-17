9d764413e811660f6e72bb648596c2b34fd481d8
Margus <margus.liinev@gmail.com>
2023-09-17 22:18:13 +0300

    solution for first non repeating char

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    const strArr = [...str];
+
+    const nonRepCharArr = strArr.reduce((acc, cur) => {
+        if (!acc.includes(cur)) {
+            acc.push(cur);
+            return acc;
+        } else {
+            return acc.filter((char) => char !== cur);
+        }
+    }, []);
+
+    const firstNonRepChar = nonRepCharArr[0];
+
+    return firstNonRepChar ? firstNonRepChar : null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
