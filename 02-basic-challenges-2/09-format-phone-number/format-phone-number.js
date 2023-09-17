f860fb1b2a840d9e27b00fde5910a7922d16ecc7
Margus <margus.liinev@gmail.com>
2023-09-18 00:25:25 +0300

    format phone number solution

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,5 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numArr) {
+    return `(${numArr.slice(0, 3).join('')}) ${numArr.slice(3, 6).join('')}-${numArr.slice(6).join('')}`;
+}
 
 module.exports = formatPhoneNumber;
