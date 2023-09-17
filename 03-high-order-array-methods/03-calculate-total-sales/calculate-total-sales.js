462c20193257f163dee9e06870bcf14f33bccbf7
Margus <margus.liinev@gmail.com>
2023-09-18 01:47:18 +0300

    calc total sales solution

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,17 @@
-function calculateTotalSalesWithTax() {}
+const products = [
+    { name: 'Apple', price: 0.5, quantity: 10 },
+    { name: 'Banana', price: 0.3, quantity: 20 },
+    { name: 'Orange', price: 0.6, quantity: 15 },
+];
+
+function calculateTotalSalesWithTax(products, taxRate) {
+    const preTaxPrice = products.reduce((acc, cur) => {
+        acc = cur.price * cur.quantity + acc;
+        return acc;
+    }, 0);
+
+    const priceWithTax = (taxRate / 100) * preTaxPrice + preTaxPrice;
+    return priceWithTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
