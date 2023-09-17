ab7aad3e8248eb6335afdcc3a4278f4f28167578
Margus <margus.liinev@gmail.com>
2023-09-18 01:38:10 +0300

    sum of even squares solution

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbersArr) {
+    return numbersArr
+        .filter((num) => num % 2 === 0)
+        .map((num) => num * num)
+        .reduce((acc, cur) => (acc += cur), 0);
+}
 
 module.exports = sumOfEvenSquares;
