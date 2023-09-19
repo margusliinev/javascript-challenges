30b6dd98bbe0049c7bce3e2ce5083d0031068cce
Margus <margus.liinev@gmail.com>
2023-09-19 19:20:45 +0300

    hashtag generator solution

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,18 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    if (!str) {
+        return false;
+    }
+    const strArr = str.split(' ');
+    const hashtag = strArr
+        .map((word) => {
+            return `${word.slice(0, 1).toUpperCase()}${word.slice(1)}`;
+        })
+        .join('');
+    if (hashtag.length > 140) {
+        return false;
+    } else {
+        return `#${hashtag}`;
+    }
+}
 
 module.exports = generateHashtag;
