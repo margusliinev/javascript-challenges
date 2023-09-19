bd79537cb0350e932cdefed62b8e7adee1ff2cc7
Margus <margus.liinev@gmail.com>
2023-09-19 20:27:02 +0300

    analyze car milage solution

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,27 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    const averageMileage = cars.reduce((acc, cur) => {
+        return (acc += cur.mileage / cars.length);
+    }, 0);
+
+    const highestMileageCar = cars.reduce((acc, cur) => {
+        if (cur.mileage > acc.mileage) {
+            return cur;
+        }
+        return acc;
+    }, cars[0]);
+
+    const lowestMileageCar = cars.reduce((acc, cur) => {
+        if (cur.mileage < acc.mileage) {
+            return cur;
+        }
+        return acc;
+    }, cars[0]);
+
+    const totalMileage = cars.reduce((acc, cur) => {
+        return (acc += cur.mileage);
+    }, 0);
+
+    return { averageMileage, highestMileageCar, lowestMileageCar, totalMileage };
+}
 
 module.exports = analyzeCarMileage;
