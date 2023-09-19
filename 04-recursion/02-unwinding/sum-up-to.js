15c3a76492b8a75705270fc686a95817e52008da
Margus <margus.liinev@gmail.com>
2023-09-20 00:01:08 +0300

    recursion, unwinding, string reversal solutions

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,23 @@
-function sumUpTo() {}
+// Recursive approach
+
+/*
+function sumUpTo(n) {
+    if (n === 1) {
+        return 1;
+    }
+
+    return n + sumUpTo(n - 1);
+}
+*/
+
+// Iterative approach
+
+function sumUpTo(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 module.exports = sumUpTo;
