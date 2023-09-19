6a37f365a8f4b439b57e36c5b9e45263ef9b3c9b
Margus <margus.liinev@gmail.com>
2023-09-19 20:04:29 +0300

    solution for valid ipv4

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,14 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+    const strArr = str.split('.');
+    if (strArr.length !== 4) {
+        return false;
+    }
+    const isValidRange = strArr.every((octet) => {
+        const num = Number(octet);
+        return num >= 0 && num <= 255 && octet[0] != 0;
+    });
+
+    return isValidRange;
+};
 
 module.exports = isValidIPv4;
