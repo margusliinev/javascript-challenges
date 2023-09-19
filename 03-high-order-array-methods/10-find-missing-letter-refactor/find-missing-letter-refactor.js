63e3acb3d0bfc67516559bddade4348cb5345502
Margus <margus.liinev@gmail.com>
2023-09-19 21:10:40 +0300

    find missing letter refactor solution

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,16 @@
-function findMissingLetter() {}
+function findMissingLetter(charArr) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const startIndex = alphabet.indexOf(charArr[0]);
+    const newAlphabet = alphabet.slice(startIndex);
+
+    const missingLetter = charArr.reduce((acc, cur, index) => {
+        if (cur !== newAlphabet[index] && !acc) {
+            acc = newAlphabet[index];
+        }
+        return acc;
+    }, '');
+
+    return missingLetter;
+}
 
 module.exports = findMissingLetter;
