e615d5b6fc0be549369960a0f096e7220f57e251
Margus <margus.liinev@gmail.com>
2023-09-19 20:51:31 +0300

    solution for password validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,9 @@
-function validatePassword() {}
+function validatePassword(password) {
+    if (password.length < 8) {
+        return false;
+    }
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/;
+    return passwordRegex.test(password);
+}
 
 module.exports = validatePassword;
