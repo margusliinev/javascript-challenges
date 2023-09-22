38d3efaeda221c5223196077270e4470a37d20a6
Margus <margus.liinev@gmail.com>
2023-09-22 22:41:09 +0300

    array-sum solution

--- 04-recursion/07-array-sum/array-sum-run.js
+++ 04-recursion/07-array-sum/array-sum-run.js
@@ -1,5 +1,5 @@
 const arraySum = require('./array-sum');
 
-const result = arraySum([1, 2, 3, 4, 5]);
+const result = arraySum([]);
 
 console.log(result);
--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,8 @@
-function arraySum() {}
+function arraySum(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    return arraySum(arr.slice(1)) + arr[0];
+}
 
 module.exports = arraySum;
