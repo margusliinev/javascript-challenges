078bd55d32e7c2ef87af63d2577bd5ddfecc2f4d
Margus Liinev <margus.liinev@gmail.com>
2023-12-12 23:05:57 +0200

    solution for basic challenges 02, 03, 04

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,15 @@
-function calculator() {}
-
+function calculator(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            throw new error('Invalid operator');
+    }
+}
 module.exports = calculator;
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,9 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+    let times = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === char) times++;
+    }
+    return times;
+}
 
 module.exports = countOccurrences;
