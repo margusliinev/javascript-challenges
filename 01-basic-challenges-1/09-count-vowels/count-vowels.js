ffcd82237bd44447a7f7899d6c14381e21051cbf
Margus Liinev <margus.liinev@gmail.com>
2023-12-13 18:21:36 +0200

    solutions for basic challenges 09 and 10

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,17 @@
-function countVowels() {}
+function countVowels(string) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const cleanedString = string.toLowerCase();
+    let count = 0;
+
+    for (let i = 0; i < cleanedString.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (vowels[j] === cleanedString[i]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 module.exports = countVowels;
