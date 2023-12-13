8034f6d6ac7058cd1cea2c8cb5ca221689cf51f7
Margus Liinev <margus.liinev@gmail.com>
2023-12-13 17:59:48 +0200

    solutions for basic challenges 06, 07, 08

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(string) {
+    let reverseString = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string[i];
+    }
+
+    return reverseString;
+}
 
 module.exports = reverseString;
--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,12 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+    const cleanedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let newString = '';
+
+    for (let i = cleanedString.length - 1; i >= 0; i--) {
+        newString += cleanedString[i];
+    }
+
+    return newString === cleanedString;
+}
 
 module.exports = isPalindrome;
