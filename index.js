d43bbc08b0fb3c9fd000bd45408a5c78e52dd3bc
Margus Liinev <margus.liinev@gmail.com>
2023-12-13 13:49:09 +0200

    added test rename script and solution for basic challenge 05

---
+++ index.js
@@ -0,0 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+async function renameTestFiles(folderPath) {
+    try {
+        const stat = await fs.stat(folderPath);
+
+        if (stat.isDirectory()) {
+            const files = await fs.readdir(folderPath);
+
+            for (const file of files) {
+                const filePath = path.join(folderPath, file);
+                await renameTestFiles(filePath);
+            }
+        } else if (folderPath.includes('-test')) {
+            const newPath = folderPath.replace('-test', '.test');
+            await fs.rename(folderPath, newPath);
+            console.log(`Renamed: ${folderPath} to ${newPath}`);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+// Before starting a new section of challenges, run this script to rename all the test files in the section directory
+// This allows you to run the tests in the current section while tests in the other directories stay disabled
+
+const folderPath = '/home/margus/code/javascript-challenges/01-basic-challenges-1';
+renameTestFiles(folderPath);
