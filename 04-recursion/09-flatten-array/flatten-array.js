8ea2d6f02189e5ffbb0069001dc9f0024e67c572
Margus Liinev <margus.liinev@gmail.com>
2023-12-18 14:12:08 +0200

    solutions for module 4, exercises 09, 10

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,16 @@
-function flattenArray() {}
+function flattenArray(arr) {
+    let result = [];
+
+    for (let element of arr) {
+        if (Array.isArray(element)) {
+            let flattenedArray = flattenArray(element.flat());
+            result.push(...flattenedArray);
+        } else {
+            result.push(element);
+        }
+    }
+
+    return result;
+}
 
 module.exports = flattenArray;
