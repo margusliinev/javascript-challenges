cdbdf17103c99a53d94c6f031c04785b09d4b8ae
Margus Liinev <margus.liinev@gmail.com>
2023-12-19 10:25:00 +0200

    solutions for module 06, exercises 04, 05, 06

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,19 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+    const words = str.toLowerCase().split(/\W+/);
+
+    const wordFrequencies = new Map();
+
+    for (const word of words) {
+        if (word === '') continue;
+
+        if (wordFrequencies.has(word)) {
+            wordFrequencies.set(word, wordFrequencies.get(word) + 1);
+        } else {
+            wordFrequencies.set(word, 1);
+        }
+    }
+
+    return wordFrequencies;
+}
 
 module.exports = wordFrequencyCounter;
--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,12 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+    let directory = new Map();
+
+    for (const entry of phoneNumbers) {
+        const [name, phoneNumber] = entry.split(':');
+        directory.set(name, phoneNumber);
+    }
+
+    return directory;
+}
 
 module.exports = phoneNumberDirectory;
