b3fc13d7406b623ac75834b1b4cc999197dc1053
Margus Liinev <margus.liinev@gmail.com>
2023-12-20 14:46:01 +0200

    solutions for module 6, exercises 08, 09, 10

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,21 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    const result = [];
+
+    for (let num of set1) {
+        if (!set2.has(num)) {
+            result.push(num);
+        }
+    }
+
+    for (let num of set2) {
+        if (!set1.has(num)) {
+            result.push(num);
+        }
+    }
+
+    return result;
+}
 
 module.exports = symmetricDifference;
--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,15 @@
-function twoSum() {}
+function twoSum(nums, target) {
+    const numSet = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        const compliment = target - nums[i];
+
+        if (numSet.has(compliment)) {
+            return [nums.indexOf(compliment), i];
+        }
+
+        numSet.add(nums[i]);
+    }
+}
 
 module.exports = twoSum;
