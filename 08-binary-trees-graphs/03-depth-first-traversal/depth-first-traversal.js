9b458214a193cd34b29f87820ce1f562b321b19c
Margus Liinev <margus.liinev@gmail.com>
2023-12-20 18:34:25 +0200

    solutions for module 8, exercises 03, 05, 08, 09, 12, 14, 15

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -34,32 +34,6 @@ function depthFirstTraversal(root) {
     return result;
 }
 
-function depthFirstTraversal(root) {
-    if (!root) {
-        return [];
-    }
-
-    const result = [];
-    const stack = new Stack();
-
-    stack.push(root);
-
-    while (!stack.isEmpty()) {
-        const current = stack.pop();
-        result.push(current.data);
-
-        if (current.right) {
-            stack.push(current.right);
-        }
-
-        if (current.left) {
-            stack.push(current.left);
-        }
-    }
-
-    return result;
-}
-
 module.exports = {
     Node,
     depthFirstTraversal,
--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -33,32 +33,6 @@ function breadthFirstTraversal(root) {
     return result;
 }
 
-function breadthFirstTraversal(root) {
-    if (!root) {
-        return [];
-    }
-
-    const result = [];
-    const queue = new Queue();
-
-    queue.enqueue(root);
-
-    while (!queue.isEmpty()) {
-        const current = queue.dequeue();
-        result.push(current.data);
-
-        if (current.left) {
-            queue.enqueue(current.left);
-        }
-
-        if (current.right) {
-            queue.enqueue(current.right);
-        }
-    }
-
-    return result;
-}
-
 module.exports = {
     Node,
     breadthFirstTraversal,
