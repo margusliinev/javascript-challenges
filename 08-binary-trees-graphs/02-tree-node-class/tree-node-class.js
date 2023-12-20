91255fd0204a4e72b598483aff1dce82e3ffc220
Margus Liinev <margus.liinev@gmail.com>
2023-12-20 17:54:21 +0200

    solutions for module 8, exercises 02, 03, 04, 05, 06

--- 08-binary-trees-graphs/02-tree-node-class/tree-node-class.js
+++ 08-binary-trees-graphs/02-tree-node-class/tree-node-class.js
@@ -1,3 +1,22 @@
-class TreeNode {}
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new TreeNode('a');
+const b = new TreeNode('b');
+const c = new TreeNode('c');
+const d = new TreeNode('d');
+const e = new TreeNode('e');
+const f = new TreeNode('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
 
 module.exports = TreeNode;
--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,63 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const queue = [];
+
+    queue.push(root);
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+        result.push(current.data);
+
+        if (current.left) {
+            queue.push(current.left);
+        }
+        if (current.right) {
+            queue.push(current.right);
+        }
+    }
+
+    return result;
+}
+
+function breadthFirstTraversal(root) {
+    if (!root) {
+        return [];
+    }
+
+    const result = [];
+    const queue = new Queue();
+
+    queue.enqueue(root);
+
+    while (!queue.isEmpty()) {
+        const current = queue.dequeue();
+        result.push(current.data);
+
+        if (current.left) {
+            queue.enqueue(current.left);
+        }
+
+        if (current.right) {
+            queue.enqueue(current.right);
+        }
+    }
+
+    return result;
+}
 
 module.exports = {
     Node,
