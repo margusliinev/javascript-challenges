a34c8305f0b0166bd0e68164d3c26fc8d477cbfe
Margus Liinev <margus.liinev@gmail.com>
2023-12-22 23:08:10 +0200

    added extra tests for binary search

--- 10-binary-search/binary-search.test.js
+++ 10-binary-search/binary-search.test.js
@@ -17,21 +17,27 @@ test('in the middle', () => {
 test('to the left of middle', () => {
     expect(binarySearch([1, 2, 3], 1)).toBe(0);
     expect(binarySearch([1, 2, 3, 4, 5], 1)).toBe(0);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 4)).toBe(3);
 });
 
 test('to the right of middle', () => {
     expect(binarySearch([1, 2, 3], 3)).toBe(2);
     expect(binarySearch([1, 2, 3, 4, 5], 5)).toBe(4);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7)).toBe(6);
 });
 
 test('to the left of even sized array', () => {
     expect(binarySearch([1, 2], 1)).toBe(0);
     expect(binarySearch([1, 2, 3, 4], 1)).toBe(0);
     expect(binarySearch([1, 2, 3, 4], 2)).toBe(1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4)).toBe(3);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)).toBe(4);
 });
 
 test('to the right of even sized array', () => {
     expect(binarySearch([1, 2], 2)).toBe(1);
     expect(binarySearch([1, 2, 3, 4], 3)).toBe(2);
     expect(binarySearch([1, 2, 3, 4], 4)).toBe(3);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6)).toBe(5);
+    expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)).toBe(6);
 });
